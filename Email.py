cd3b1a03f5ed8464c22b085bf6a493ac454f46af
vaakinmade <vickeyakinmade22@gmail.com>
2017-10-07 16:30:18 +0100

    Wrote Email client class. Implemented method for opening/reading email file into html.

--- Email.py
+++ Email.py
@@ -10,3 +10,16 @@ from email.mime.text import MIMEText
 from datetime import datetime
 from GMAIL_PWD import GMAIL_PWD
 
+
+
+class Email():
+	def __init__(self, msg_file):
+		self.msg_file = msg_file
+
+	def send_gmail():
+		with open(self.msg_file, mode = "rb") as message:
+			msg = MIMEText(message.read(), "html", "html")
+
+	msg["Subject"] = "Hourly Weather {}".format(datetime.now().strftime("%Y-%m-%d %H:%M"))
+	msg["From"] = "vickeyakinmade22@gmail.com"
+	msg["To"] = 'victorakinmade23@gmail.com, vickeyakinmade22@gmail.com'
