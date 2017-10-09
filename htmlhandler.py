bc03151875cbe00213b12ed730aa5db3479db0b9
vaakinmade <vickeyakinmade22@gmail.com>
2017-10-09 23:58:40 +0100

    [WIP] Refactoring html for presentation. Retrieving teams crest url. converting timezone aware datetimes.

--- htmlhandler.py
+++ htmlhandler.py
@@ -1,30 +1,48 @@
 from datetime import datetime
+import iso8601
 
 
 class HtmlHandler():
 	@classmethod
-	def create_html_file(cls, data_dict, icon_url, html_file):
-    
-	    alt_var = data_dict['weather']
-	   
+	def create_html_file(cls, list_dict, crest_url, html_file):
+		utc_time = iso8601.parse_date('2012-11-01T04:16:13-04:00')
+
+		print(datetime.strftime(utc_time, "%d %b %y"))
 	    with open(html_file, mode='w') as outfile:
-	        outfile.write( '\t<tr><td align="center">' + datetime.now()
-	                       .strftime("%Y-%m-%d %H:%M:%S") +'</td></tr><br>\n' )
-	        outfile.write( '<img alt={} src={}>'.format(alt_var, icon_url))
-	        outfile.write( '<br><span style="color:blue"><b>\tWeather Data:</b>\n' )
-	        outfile.write( '<br>')    
+	        outfile.write('\t<tr><td align="center">'
+	        	+ datetime.strftime(utc_time, "%d %b %y")
+	        	+'</td></tr><br>\n')
+	        outfile.write('<img alt={} src={}>'.format(
+	        	"Premier League Fixtures", crest_url))
+	        outfile.write('''<br>
+	        	<span style="color:blue"><b>\tEPL Upcoming Fixtures:</b>\n''')
+	        outfile.write('<br>')    
 	            
-	        outfile.write( '<html><table border=1>\n' )
-	        for key, value in data_dict.items():
-	            outfile.write( '<tr><td><b><span style="color:black">{:s}</b></td><td align="left"> \
-	                <span style="color:blue"><b>{:s}</b></td></tr>\n'.format(key, value))
-	        outfile.write( '</table></html>\n' )
+	        outfile.write('<html><table border=1>\n')
+			for data_dict in list_dict["fixtures"]:
+		        for key in data_dict.keys():
+		            outfile.write('''<tr>
+		            	<td><b><span style="color:blue">{0}</b></td>
+		            	<td>
+		            		<strong>
+		            			<span style="color:black">{2:%H:%M}</span>
+		            		</strong>
+		            	</td>
+		            	<td align="left">
+		            		<span style="color:blue"><b>{1}</b>
+		            	</td>
+		            	</tr>\n'''.format(
+		            		key["homeTeamName"],
+		            		key["awayTeamName"],
+		            		key["date"]))
+		        outfile.write('</table></html>\n')
 
 	    return outfile
 
 # pseudo test
 if __name__ == '__main__':
-    from weather import Weather
-    weather_dict, icon = Weather('KLAX').get_data_iconurl()
-    HtmlHandler.create_html_file(weather_dict, icon, "Test_Email_File.html")
+    from footyapi import FootballDataAPI
+    obj = FootballDataAPI()
+    list_dict, crest = obj.retrieve_previous_fixtures(), obj.get_club_crest(66)
+    HtmlHandler.create_html_file(list_dict, crest, "Footy_Email_File.html")
 
