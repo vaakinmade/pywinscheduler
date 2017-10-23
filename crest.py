8e9ae81f8a7ee49f32bd7db21aa9421529892149
vaakinmade <vickeyakinmade22@gmail.com>
2017-10-23 05:19:30 +0100

    Optimised Crest class for high performance. It now starts by doing local file lookup for crest. if not found it proceeds to redis key store to check retrieve existing crest url. If absent it then makes an api call to get new crest data, which stores in redis key store. It finally saves the file locally and uploads it to AWS S3 for remote access.

--- crest.py
+++ crest.py
@@ -11,6 +11,11 @@ from decouple import config
 
 
 class Crest():
+	S3_ACCESS_KEY = config('AWS_ACCESS_KEY_ID', cast=str)
+	S3_SECRET_KEY = config('AWS_SECRET_ACCESS_KEY', cast=str)
+	S3_BUCKET = config('AWS_STORAGE_BUCKET_NAME', cast=str)
+	HOST_REGION = "s3.us-east-2.amazonaws.com"
+
 	@classmethod
 	def upload_to_s3(cls, image_file):
 		S3_ACCESS_KEY = config('AWS_ACCESS_KEY_ID', cast=str)
@@ -18,9 +23,13 @@ class Crest():
 		S3_BUCKET = config('AWS_STORAGE_BUCKET_NAME', cast=str)
 		HOST_REGION = "s3.us-east-2.amazonaws.com"
 
+
 		f = open(image_file, 'rb')
-		conn = boto.connect_s3(S3_ACCESS_KEY, S3_SECRET_KEY, host=HOST_REGION)
-		bucket = conn.get_bucket(S3_BUCKET)
+		conn = boto.connect_s3(cls.S3_ACCESS_KEY,
+			cls.S3_SECRET_KEY,
+			host=cls.HOST_REGION)
+
+		bucket = conn.get_bucket(cls.S3_BUCKET)
 
 		k = Key(bucket)
 		filename_base = image_file.split("/")
@@ -28,7 +37,7 @@ class Crest():
 		file_size = k.set_contents_from_file(f)
 
 		s3_file = "https://s3.us-east-2.amazonaws.com/{}/crests/{}".format(
-			S3_BUCKET,
+			cls.S3_BUCKET,
 			filename_base[-1])
 		return s3_file
 
@@ -41,15 +50,30 @@ class Crest():
 
 	@classmethod
 	def team_crest(cls, competition_id, home_id, away_id):
-		clubs = FootballDataAPI().get_clubs(competition_id)
+		home_crest = os.path.isfile("img/crests/{}.png".format(home_id))
+		away_crest = os.path.isfile("img/crests/{}.png".format(away_id))
 
-		# attempt to retrieve crest from crest list
-		home_crest = clubs.get(str(home_id).encode('utf-8'))
-		away_crest = clubs.get(str(away_id).encode('utf-8'))
+		if home_crest and away_crest:
+			s3_home_url = "https://{}/{}/crests/{}.png".format(
+			cls.HOST_REGION,
+			cls.S3_BUCKET,
+			home_id)
 
-		home_crest = home_crest.decode('utf-8')
-		away_crest = away_crest.decode('utf-8')
-		
-		s3_home_url = cls.svg2png(home_crest, home_id)
-		s3_away_url = cls.svg2png(away_crest, away_id)
+			s3_away_url = "https://{}/{}/crests/{}.png".format(
+			cls.HOST_REGION,
+			cls.S3_BUCKET,
+			away_id)
+			
+		if home_crest is False:
+			print("home crest is absent... processing new one")
+			clubs = FootballDataAPI().get_clubs(competition_id)
+			svg_url = clubs.get(str(home_id).encode('utf-8'))
+			s3_home_url = cls.svg2png(svg_url.decode('utf-8'), home_id)
+						
+		if away_crest is False:
+			print("away crest is absent... processing new one")
+			clubs = FootballDataAPI().get_clubs(competition_id)
+			svg_url = clubs.get(str(home_id).encode('utf-8'))
+			s3_away_url = cls.svg2png(svg_url.decode('utf-8'), away_id)
+				
 		return s3_home_url, s3_away_url
