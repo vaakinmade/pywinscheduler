e975bd5b9037ffdacf201c9dc85b0601de0da391
vaakinmade <vickeyakinmade22@gmail.com>
2017-10-23 03:58:37 +0100

    team_crest method serves as entry point and returns s3 crest urls.

--- crest.py
+++ crest.py
@@ -1,34 +1,43 @@
 # installed pycairo using windows binary wheel file
 #installed cffi to install cairocffi
 # installed cairocffi to install cairosvg
+import os
 from footyapi import FootballDataAPI
 
 import cairosvg
-import tinys3
+import boto
+from boto.s3.key import Key
 from decouple import config
 
 
 class Crest():
 	@classmethod
-	def upload_to_s3(cls, file):
+	def upload_to_s3(cls, image_file):
 		S3_ACCESS_KEY = config('AWS_ACCESS_KEY_ID', cast=str)
 		S3_SECRET_KEY = config('AWS_SECRET_ACCESS_KEY', cast=str)
 		S3_BUCKET = config('AWS_STORAGE_BUCKET_NAME', cast=str)
-		conn = tinys3.Connection(
-			S3_ACCESS_KEY,
-			S3_SECRET_KEY,
-			tls=True)
-		print("File", file)
-		f = open('{}'.format(file), 'rb')
-		conn.upload('crests/{}'.format(file), f, S3_BUCKET)
+		HOST_REGION = "s3.us-east-2.amazonaws.com"
+
+		f = open(image_file, 'rb')
+		conn = boto.connect_s3(S3_ACCESS_KEY, S3_SECRET_KEY, host=HOST_REGION)
+		bucket = conn.get_bucket(S3_BUCKET)
+
+		k = Key(bucket)
+		filename_base = image_file.split("/")
+		k.key = "crests/" + filename_base[-1]
+		file_size = k.set_contents_from_file(f)
+
+		s3_file = "https://s3.us-east-2.amazonaws.com/{}/crests/{}".format(
+			S3_BUCKET,
+			filename_base[-1])
+		return s3_file
 
 	@classmethod
 	def svg2png(cls, svg_url, team_id):
 		png_file = cairosvg.svg2png(url=svg_url,
 			write_to="img/crests/{}.png".format(team_id))
-
-		cls.upload_to_s3("crests/{}.png".format(team_id))
-		return png_file
+		result = cls.upload_to_s3("img/crests/{}.png".format(team_id))
+		return result
 
 	@classmethod
 	def team_crest(cls, competition_id, home_id, away_id):
@@ -41,6 +50,6 @@ class Crest():
 		home_crest = home_crest.decode('utf-8')
 		away_crest = away_crest.decode('utf-8')
 		
-		print(cls.svg2png(home_crest, home_id),
-			cls.svg2png(away_crest, away_id))
-		return cls.svg2png(home_crest), cls.svg2png(away_crest)
+		s3_home_url = cls.svg2png(home_crest, home_id)
+		s3_away_url = cls.svg2png(away_crest, away_id)
+		return s3_home_url, s3_away_url
