07f46a4f743537be32cb1fe249569b435476579a
vaakinmade <vickeyakinmade22@gmail.com>
2017-10-27 23:32:29 +0100

    Implemented scheduling job to run new htmlhandler methods.

--- winservice.py
+++ winservice.py
@@ -5,7 +5,7 @@ Run the scheduled task as a windows service
 """
 
 
-from weather import Weather
+from footyapi import FootballDataAPI
 from htmlhandler import HtmlHandler
 from emailserver import EmailService
 from collections import OrderedDict
@@ -15,3 +15,46 @@ import schedule
 import win32service         
 import win32serviceutil  
 import win32event
+
+
+class FootyAlertTaskSvc(win32serviceutil.ServiceFramework):  
+    _svc_name_ = "PyFootyAlert"    
+    _svc_display_name_ = "Python Scheduling Service for footy inboxer"   
+    _svc_description_ = "This Python service schedules tasks"  
+      
+    def __init__(self, args):  
+        win32serviceutil.ServiceFramework.__init__(self,args)  
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)  
+        
+    def SvcDoRun(self):  
+        def job():
+            # weather_dict, icon = get_weather_data('KLAX')  
+            # weather_dict_ordered = OrderedDict(sorted(weather_dict.items()))
+
+            # email_file = "Email_File.html"
+            # create_html_report(weather_dict_ordered, icon, email_file)
+            # send_gmail(email_file)
+
+            obj = FootballDataAPI()
+			HtmlHandler().create_html_file(
+				obj.retrieve_matchday_fixtures(445),
+				obj.single_team_fixtures(66),
+				obj.latest_competition_results(445)
+				)
+			email_file = "Email_File.html"
+            EmailService(email_file).send_email()
+        
+        #schedule.every().hour.do(job)
+        schedule.every(1).minutes.do(job)   # for dev use 1 minute
+        
+        rc = None
+        while rc != win32event.WAIT_OBJECT_0:
+            schedule.run_pending()
+            rc = win32event.WaitForSingleObject(self.hWaitStop, 5000)  
+            
+    def SvcStop(self):  
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)  
+        win32event.SetEvent(self.hWaitStop)  
+          
+if __name__ == '__main__':  
+    win32serviceutil.HandleCommandLine(PythonTaskSvc)
